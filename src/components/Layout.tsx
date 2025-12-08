ccbc4a133c67c2903e1c80e90b07dd4730ad891c
OxBryte <silascyrax@gmail.com>
2025-12-08 17:24:44 +0100

    refactor: improve layout structure and styling for better responsiveness and user experience

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,14 @@ import ConnectMenu from "./ConnectMenu";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="p-3">
-      <div className="flex items-center gap-2 justify-between w-full">
-        <img src="/logo.png" alt="logo" className="w-10 h-10" />
-        <ConnectMenu />
+    <div className="p-3 w-full mx-auto h-screen flex items-center justify-center">
+      <div className="w-full mx-auto max-w-xl flex flex-col gap-4">
+        <div className="flex items-center gap-2 justify-between w-full">
+          <p>Logo</p>
+          <ConnectMenu />
+        </div>
+        {children}
       </div>
-      {children}
     </div>
   );
 }
