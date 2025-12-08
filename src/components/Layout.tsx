8409e495c279436fbfa8cdd65064b29d16c4af21
OxBryte <silascyrax@gmail.com>
2025-12-08 18:31:42 +0100

    feat: add react-icons and sonner dependencies, and introduce a new gift SVG asset

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,14 @@ import ConnectMenu from "./ConnectMenu";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="p-3 w-full mx-auto h-screen flex items-center justify-center">
-      <div className="w-full mx-auto max-w-xl flex flex-col gap-4">
+      <div className="w-full mx-auto max-w-[440px] flex flex-col gap-4">
         <div className="flex items-center gap-2 justify-between w-full">
           <p>Logo</p>
           <ConnectMenu />
         </div>
-        {children}
+        <div className="w-full bg-white rounded-lg p-5 border border-gray-100 h-[50vh] flex items-center justify-center">
+          {children}
+        </div>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { WagmiProvider } from "wagmi";
-import { AppKitProvider } from "@reown/appkit/react";
 import App from "./App.tsx";
 import { config } from "./config/wagmi.ts";
 
 import "./index.css";
-import { base } from "viem/chains";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
-      <AppKitProvider
-        projectId="33df6a21ce6237ff3dfb82d3cd1d2c77"
-        networks={[base]}
-      >
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </AppKitProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster />
+      </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>
 );
